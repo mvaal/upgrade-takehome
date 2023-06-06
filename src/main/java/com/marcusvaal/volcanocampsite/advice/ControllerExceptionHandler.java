1b53cfccb04e6d0398a605af1fd68a1e186fee12
mvaalexp <mvaal@expediagroup.com>
2023-06-05 20:37:05 -0500

    Fixed broken queries in booking-service.http
    Updated error response to match what spring web returns
    
    Signed-off-by: mvaalexp <mvaal@expediagroup.com>

--- src/main/java/com/marcusvaal/volcanocampsite/advice/ControllerExceptionHandler.java
+++ src/main/java/com/marcusvaal/volcanocampsite/advice/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.marcusvaal.volcanocampsite.advice;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.format.DateTimeParseException;
 import java.util.List;
@@ -25,21 +25,21 @@ public class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
         BindingResult result = ex.getBindingResult();
         List<String> fieldErrors = result.getFieldErrors().stream()
                 .map(f -> String.format("%s - %s", f.getField(), f.getDefaultMessage()))
                 .toList();
-        return new ResponseEntity<>(new ErrorResponse(String.join("\n", fieldErrors)), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST, String.join("\n", fieldErrors), request.getServletPath()), HttpStatus.BAD_REQUEST);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = {HttpMessageNotReadableException.class})
-    public ResponseEntity<ErrorResponse> httpMessageNotReadableException(HttpMessageNotReadableException ex, WebRequest request) {
+    public ResponseEntity<ErrorResponse> httpMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request) {
         if (ex.getCause() instanceof InvalidFormatException) {
             if (ex.getCause().getCause() instanceof DateTimeParseException) {
                 String errorMessage = String.format("Date format must be MM/dd/yyyy: %s", ex.getCause().getCause().getMessage());
-                return new ResponseEntity<>(new ErrorResponse(errorMessage), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage, request.getServletPath()), HttpStatus.BAD_REQUEST);
             }
         }
         return handleAllExceptions(ex, request);
@@ -47,25 +47,25 @@ public class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public final ResponseEntity<ErrorResponse> dataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+    public final ResponseEntity<ErrorResponse> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
         if (ex.getCause() instanceof ConstraintViolationException) {
             if (ex.getCause().getMessage().contains("RESERVATION")) {
                 String errorMessage = "Booking is attempting to be scheduled on an existing reserved date";
-                return new ResponseEntity<>(new ErrorResponse(errorMessage), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage, request.getServletPath()), HttpStatus.BAD_REQUEST);
             }
         }
         return handleAllExceptions(ex, request);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public final ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, WebRequest request) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    public final ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request.getServletPath()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+    public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, HttpServletRequest request) {
         logger.error("Unhandled Error Exception: {}", request, ex);
         String errorMessage = "Unknown Exception, please contact support.";
-        return new ResponseEntity<>(new ErrorResponse(errorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, request.getContextPath()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/marcusvaal/volcanocampsite/advice/ErrorResponse.java
+++ src/main/java/com/marcusvaal/volcanocampsite/advice/ErrorResponse.java
@@ -1,7 +1,9 @@
 package com.marcusvaal.volcanocampsite.advice;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
@@ -10,5 +12,18 @@ import java.time.LocalDateTime;
 public class ErrorResponse {
     private final LocalDateTime timestamp = LocalDateTime.now();
 
+    @JsonIgnore
+    private final HttpStatus httpStatus;
+
     private final String message;
+
+    private final String path;
+
+    public int getStatus() {
+        return httpStatus.value();
+    }
+
+    public String getError() {
+        return httpStatus.getReasonPhrase();
+    }
 }
